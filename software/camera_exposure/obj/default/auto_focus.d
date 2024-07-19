2ef5124583fc8878697aed97a5df89935f08ec37
Pinqian Jin <jin.pinqian@outlook.com>
2024-07-19 21:02:13 +0100

    initial commit

---
+++ software/camera_exposure/obj/default/auto_focus.d
@@ -0,0 +1,52 @@
+obj/default/auto_focus.o: auto_focus.c I2C_core.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\alt_types.h \
+ terasic_includes.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_flash.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_flash_types.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_flash_dev.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_llist.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\priv\alt_dev_llist.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_cache.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_flash_types.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\io.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\alt_types.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\drivers\inc\altera_avalon_pio_regs.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_irq.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\nios2.h \
+ e:/summer_project/camera_module_with_edge/camera_starter/EEE2Rover/DE10_LITE_D8M_VIP_16/software/camera_exposure_bsp/system.h \
+ e:/summer_project/camera_module_with_edge/camera_starter/EEE2Rover/DE10_LITE_D8M_VIP_16/software/camera_exposure_bsp/linker.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_alarm.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_llist.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\priv\alt_alarm.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_timestamp.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\drivers\inc\altera_avalon_timer.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_dev.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_warning.h \
+ e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_stdio.h \
+ auto_focus.h
+I2C_core.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\alt_types.h:
+terasic_includes.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_flash.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_flash_types.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_flash_dev.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_llist.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\priv\alt_dev_llist.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_cache.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_flash_types.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\io.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\alt_types.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\drivers\inc\altera_avalon_pio_regs.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_irq.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\nios2.h:
+e:/summer_project/camera_module_with_edge/camera_starter/EEE2Rover/DE10_LITE_D8M_VIP_16/software/camera_exposure_bsp/system.h:
+e:/summer_project/camera_module_with_edge/camera_starter/EEE2Rover/DE10_LITE_D8M_VIP_16/software/camera_exposure_bsp/linker.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_alarm.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_llist.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\priv\alt_alarm.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_timestamp.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\drivers\inc\altera_avalon_timer.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_dev.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_warning.h:
+e:\summer_project\camera_module_with_edge\camera_starter\eee2rover\de10_lite_d8m_vip_16\software\camera_exposure_bsp\hal\inc\sys\alt_stdio.h:
+auto_focus.h:
